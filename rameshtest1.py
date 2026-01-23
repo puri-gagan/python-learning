4140d4175cf84ac2f075917097666f2a56d6f70a
bohara18 <bohararamesh@gmail.com>
2026-01-22 21:08:19 -0600

    new added code

--- rameshtest1.py
+++ rameshtest1.py
@@ -1,1 +1,13 @@
 print(" this is now test file and it has small code change")
+list = [1,2,3,4,5,6]
+print (type(list))
+# empty dictionary
+my_dict = {}
+
+# dictionary with values
+student = {
+    "name": "Alice",
+    "age": 21,
+    "course": "Computer Science"
+}
+print(student)
