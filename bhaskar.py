98b683855049412c5e045c2d433fd76ae5fd8c09
Bhaskar KC <bhaskarkc2000@gmail.com>
2026-01-23 08:11:31 +0545

    second commit

--- bhaskar.py
+++ bhaskar.py
@@ -1,1 +1,18 @@
+"""Simple script for learning Python collaboration."""
 print("Learning python collaboration")
+
+# Task:
+  # git clone ssh-link
+  # open with vs code
+  # in terminal
+    # git checkout -b bhaskar
+    # add new file with your name --> bhaskar.py
+    # make some changes there
+
+  # git add .
+  # git commit -m "message"
+  # git push origin bhaskar
+
+  # Then pull request
+
+  # note: setting -> collaborators -> add github id
