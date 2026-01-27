dafd4d67cc7e169967e6ea3026421fcffa49ad49
Sairash Chaudhary <sairash0323@gmail.com>
2026-01-27 06:52:11 +0545

    fixed logical bug

--- sairash.py
+++ sairash.py
@@ -39,10 +39,12 @@ import math
 def is_prime(num):
     if num < 2:
         return False
-    elif num % 2 == 0:
+    if num == 2:
+        return True
+    if num % 2 == 0:
         return False
     else:
-        for i in range(3, int(math.sqrt(num))+1, 2):
+        for i in range(3, int(math.sqrt(num)) + 1, 2):
             if num % i == 0:
                 return False
     return True
