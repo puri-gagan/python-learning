b8bb52c5c04dda4733a7a276b531555ce7330869
Sairash Chaudhary <sairash0323@gmail.com>
2026-02-10 17:53:20 +0545

    prime no. generator

---
+++ sairash/prime_generator.py
@@ -0,0 +1,23 @@
+def is_prime(n):
+    if n < 2:
+        return False
+
+    for i in range(2, int(n ** 0.5) + 1):
+        if n % i == 0:
+            return False
+
+    return True
+
+
+def prime_generator():
+    num = 2
+    while True:
+        if is_prime(num):
+            yield num
+        num += 1
+
+
+gen = prime_generator()
+
+for _ in range(3):
+    print(next(gen))
